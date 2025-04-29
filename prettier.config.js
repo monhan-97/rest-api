3d6816390b02714abc3b9880b9ecdf919cea6326
黄梦汉 <menghan.huang@innodealing.com>
2025-04-29 17:49:01 +0800

    chore: add prettier-config

---
+++ prettier.config.js
@@ -0,0 +1,7 @@
+import prettierConfig from '@refer-utils/prettier-config';
+
+/**
+ * @see https://prettier.io/docs/configuration
+ * @type {import("prettier").Config}
+ */
+export default prettierConfig;
